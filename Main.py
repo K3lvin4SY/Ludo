923b7156e824bebc9db6ed982070ce9913150339
K3lvin4SY <kelvinbrahe@gmail.com>
2022-05-05 22:38:26 +0200

    file delete
    removed test and old files

--- Main.py
+++ Main.py
@@ -1,125 +1,259 @@
-from turtle import width
+from codeop import CommandCompiler
 import pygame
-import sys
-
-from setuptools import Command
+from colors import colors
+from gui import *
+import json
 
 pygame.init()
 
 #https://www.petercollingridge.co.uk/tutorials/pygame-physics-simulation/creating-pygame-window/
-def test():
-    print("lol")
-class WindowHandler:
+
+
+#https://www.youtube.com/watch?v=aqhp_-CbE_w
+class Screen():
+    def __init__(self, title, width=1280, height=720, fill=colors["Secondary"]) -> None:
+        
+        self.properties = Properties(width, height)
+
+        self.title = title
+        self.width = self.properties.width
+        self.height = self.properties.height
+        self.fill = fill
+        self.enabled = False
+    
+    def enable(self):
+        pygame.display.set_caption(self.title)
+        self.enabled = True
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.screen.fill(self.fill)
+
+    def disable(self):
+        self.enabled = False
+    
+    def checkState(self):
+        return self.enabled
+
+    def update(self):
+        if not self.enabled:
+            self.screen.fill(self.fill)
+        
+    def getTitle(self):
+        return self.screen
+
+class WindowSystem:
 
     def __init__(self) -> None:
 
+        self.items = {}
         self.properties = Properties(1280, 720)
 
-        self.buttons = {}
+        #self.mainScreen = pygame.display.set_mode((self.properties.width, self.properties.height))
 
-        self.mainScreen = pygame.display.set_mode((self.properties.width, self.properties.height))
-
-        pygame.display.flip()
+        #pygame.display.flip()
         self.running = True
 
-        pygame.draw.rect(self.mainScreen, (195, 200, 219), pygame.Rect(0, 0, self.properties.width, self.properties.height))
-        pygame.draw.rect(self.mainScreen, (255,255,0), pygame.Rect(30, 30, 60, 60))
+        #pygame.draw.rect(self.mainScreen, (195, 200, 219), pygame.Rect(0, 0, self.properties.width, self.properties.height))
+        #pygame.draw.rect(self.mainScreen, (255,255,0), pygame.Rect(30, 30, 60, 60))
+        startScreen = Screen("Start Screen")
+        sGameOptionsScreen = Screen("Singleplayer Game Options")
+        mGameOptionsScreen = Screen("Multiplayer Game Options")
+        gameScreen = Screen("Game")
 
-        self.main()
-        
+        self.screens = {
+            "main":startScreen,
+            "sgo":sGameOptionsScreen,
+            "mgo":mGameOptionsScreen,
+            "gs":gameScreen
+        }
+
+        self.screensFunc = {
+            "main":self.main,
+            "sgo":self.singleGameOptScn,
+            "mgo":self.multiGameOptScn,
+            "gs":self.gameScn
+        }
+
+        startScreen.enable()
+
+        self.main(startScreen.getTitle())
 
 
         while self.running:
             
+            
+
+
             for event in pygame.event.get():
                 self.update(event)
                 if event.type == pygame.QUIT:
                     self.running = False
     
-    def main(self):
+    def main(self, scn):
         
-        self.win = "main"
-        self.singlePlayerBtn = self.addButton(Button( self.properties, 100, 50, centerX=True, centerY=True, y=-100, text='Tesadwawdwdawt'))
-        self.singlePlayerBtn.draw(self.mainScreen)
-        self.multiPlayerBtn = self.addButton(Button(self.properties, 100, 50, centerX=True, centerY=True, y=100, text='Test', color=(255,0,255), command=test))
+        self.display = "main"
+        self.titleTB = self.addTextBox(TextBox(self.properties, 500, 70, centerX=True, y=100, text='Ludo', color=colors["Primary2"]))
+        self.titleTB.draw(scn)
+        self.singlePlayerBtn = self.addTextBox(TextBox(self.properties, 230, 60, centerX=True, centerY=True, y=0, text='Singleplayer', color=colors["DarkGrey"], hoverColor=colors["White"], command=lambda x="sgo": self.changeScreen(x)))
+        self.singlePlayerBtn.draw(scn, outline=colors["Primary1"], size=40)
+        self.multiPlayerBtn = self.addTextBox(TextBox(self.properties, 230, 60, centerX=True, centerY=True, y=100, text='Multiplayer', color=colors["DarkGrey"], hoverColor=colors["White"], command=lambda x="mgo": self.changeScreen(x)))
         
-        self.multiPlayerBtn.draw(self.mainScreen, outline=(0,0,255))
+        self.multiPlayerBtn.draw(scn, outline=colors["Primary1"], size=40)
 
-    def changeScreen(self, start, end):
-        pass # Change screen by looping throu items
+    def multiGameOptScn(self, scn):
+        """GUI for multiplayer options
+
+        Args:
+            scn (Screen): the linked screen to this gui layout
+        """
+        self.display = "mgo"
+        # Title
+        self.titleTB = self.addTextBox(TextBox(self.properties, 500, 70, centerX=True, y=100, text='Multiplayer Options', color=colors["Primary2"]))
+        self.titleTB.draw(scn)
+
+        # Player selection
+        self.botsSelect = self.addTextBox(Selection(self.properties, ["1", "2", "3", "4"], True, True, title="Players:"))
+        self.botsSelect.draw(scn, 40)
+
+        # Start Btn
+        self.backBtn = self.addTextBox(TextBox(self.properties, 150, 60, color=colors["Primary1"], centerX=True, centerY=False, y="max-20", x=0, text='Start', command=lambda: self.startGame()))
+        self.backBtn.draw(scn)
+
+        # Back Btn
+        self.backBtn = self.addTextBox(TextBox(self.properties, 150, 60, color=colors["Primary1"], centerX=False, centerY=False, y="max-20", x=20, text='Back', command=lambda x="main": self.changeScreen(x)))
+        self.backBtn.draw(scn)
+
+    def singleGameOptScn(self, scn):
+        """GUI for singleplayer options
+
+        Args:
+            scn (Screen): the linked screen to this gui layout
+        """
+        self.display = "sgo"
+        # Title
+        self.titleTB = self.addTextBox(TextBox(self.properties, 500, 70, centerX=True, y=100, text='Singleplayer Options', color=colors["Primary2"]))
+        self.titleTB.draw(scn)
+
+        # Player selection
+        self.botsSelect = self.addTextBox(Selection(self.properties, ["1", "2", "3"], True, True, title="Bots:"))
+        self.botsSelect.draw(scn, 40)
+
+        # Start Btn
+        self.backBtn = self.addTextBox(TextBox(self.properties, 150, 60, color=colors["Primary1"], centerX=True, centerY=False, y="max-20", x=0, text='Start', command=lambda: self.startGame()))
+        self.backBtn.draw(scn)
+
+        # Back Btn
+        self.backBtn = self.addTextBox(TextBox(self.properties, 150, 60, color=colors["Primary1"], centerX=False, centerY=False, y="max-20", x=20, text='Back', command=lambda x="main": self.changeScreen(x)))
+        self.backBtn.draw(scn)
     
-    def update(self, event):
-        pygame.display.flip()
+    def gameScn(self, scn):
+        self.display = "gs"
+        # Back Btn
+        self.backBtn = self.addTextBox(TextBox(self.properties, 150, 60, color=colors["Primary1"], centerX=False, centerY=False, y="max-20", x=20, text='Back', command=lambda x="main": self.changeScreen(x)))
+        self.backBtn.draw(scn)
+
+    def changeScreen(self, to):
+        """method for changing screens by enabaling the new and disableing the old
+
+        Args:
+            to (string): gets the string code of a screen.
+        """
+        if to != self.display: # if screen code id not current
+            if to != self.screens["gs"]:
+                self.participants = 0
+            self.items = {} # Clear items from last Screen
+            self.screens[to].enable() # enable requested screen
+            self.screens[self.display].disable() # disable current
+            self.screensFunc[to](self.screens[to].getTitle()) # activate new screen
+    
+    def update(self, event): # method that gets called aas quicly as possible (main loop)
+        
+        
         pos = pygame.mouse.get_pos()
 
+        # update items here
+        pygame.display.flip()
+        self.screens[self.display].update()
+        
         if event.type == pygame.MOUSEMOTION:
-            for btn in self.buttons:
-                if self.buttons[btn] == self.win:
-                    btn.isOver(pos)
-                    print("d")
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            for btn in self.buttons:
-                if self.buttons[btn] == self.win:
-                    if btn.isOver(pos) == True:
-                        btn.command()
-        pass
-
-    def addButton(self, btn):
-        self.buttons[btn] = self.win
-        return btn
-
-class Pawn:
-    def __init__(self, color) -> None:
-        self.color = color
-        pass
-
-#https://www.youtube.com/watch?v=4_9twnEduFA&ab_channel=TechWithTim
-class Button():
-    def __init__(self, properties, width, height, centerX=False, centerY=False, x=0 , y=0, color=(255,255,0), text='', hoverColor=(0,255,0), command=None):
-        if centerX == True:
-            self.x = int((properties.width - width) / 2)
-            self.x += x
-        else:
-            self.x = x
-        if centerY == True:
-            self.y = int((properties.height - height) / 2)
-            self.y += y
-        else:
-            self.y = y
-
-        self.color = color
-        self.originalColor = color
-        self.hoverColor = hoverColor
-        self.width = width
-        self.height = height
-        self.text = text
-
-        if command != None:
-            self.command = lambda: command
-
-    def draw(self,win,outline=None, size=60):
-        #Call this method to draw the button on the screen
-        if outline:
-            pygame.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4), 0, border_radius=8)
             
-        pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height), 0, border_radius=8)
+            for it in self.items:
+                if self.items[it][0] == self.display and self.items[it][1] == "textbox":
+                    it.isOver(pos)
+                elif self.items[it][0] == self.display and self.items[it][1] == "select":
+                    it.isOver(pos)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for it in self.items:
+                if self.items[it][0] == self.display and self.items[it][1] == "textbox":
+                    if it.isOver(pos) == True:
+                        if it.command != None:
+                            it.command()
+                            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                            break
+                elif self.items[it][0] == self.display and self.items[it][1] == "select":
+                    if it.isOver(pos) == True:
+                        self.participants = it.getOver(pos).command()
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                        break
+
+        pass
         
-        if self.text != '':
-            font = pygame.font.SysFont('comicsans', size)
-            text = font.render(self.text, 1, (0,0,0))
-            win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+    def addTextBox(self, tb):
+        """adds the textbox to a dictionary
+
+        Args:
+            tb (TextBox): the textBox created
+
+        Returns:
+            TextBox: returns the given textbox
+        """
+        if isinstance(tb, Selection):
+            self.items[tb] = [self.display, "select"]
+        else:
+            self.items[tb] = [self.display, "textbox"]
+        return tb
+
+    def startGame(self):
+        if self.participants == 0:
+            return
+        print("Starting Game along with " + str(self.participants) + " participants!")
+        self.changeScreen("gs")
+        pass
+
+class GamePlatform:
+    def __init__(self, mode, participants, size) -> None:
+        self.mode = mode
+        self.participants = participants
+        playerColors = ["Red", "Orange", "Yellow", "Green"]
+        self.players = {}
+        if self.mode == "mgo":
+            for i in range(self.participants):
+                self.players[playerColors[i]] = Player(playerColors[i])
+        elif self.mode == "sgo":
+            self.players[playerColors[0]] = Player(playerColors[0])
+            for i in range(self.participants):
+                self.players[playerColors[i+1]] = Player(playerColors[i+1], bot=True)
 
         
+    
+    def draw(self, display, size):
+        pass
 
-    def isOver(self, pos):
-        #Pos is the mouse position or a tuple of (x,y) coordinates
-        if pos[0] > self.x and pos[0] < self.x + self.width:
-            if pos[1] > self.y and pos[1] < self.y + self.height:
+class Player():
+    def __init__(self, color, bot=False) -> None:
+        self.color = color
+        self.bot = bot
+        self.pawns = []
+        for i in range(4):
+            self.pawns.append(Pawn(self, self.color))
+        pass
 
-                self.color = self.hoverColor
-                return True
-        self.color = self.originalColor
-        return False
+class Pawn(Player):
+    def __init__(self, parent, color) -> None:
+        super().__init__(self, parent)
+        self.color = color
+        pass
+
 
 class Properties():
     def __init__(self, width, height) -> None:
@@ -127,4 +261,4 @@ class Properties():
         self.height =height
 
 
-game = WindowHandler()
+game = WindowSystem()
